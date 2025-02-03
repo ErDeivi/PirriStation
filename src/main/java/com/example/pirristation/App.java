b11d5a7ec3a30f0279c34696f822367af43b9fd9
David <dramirezg03@educarex.es>
2025-02-03 19:20:46 +0100

    estructura basica

---
+++ src/main/java/com/example/pirristation/App.java
@@ -0,0 +1,4 @@
+package com.example.pirristation;
+
+public class App {
+}
---
+++ src/main/java/com/example/pirristation/Controladores/ModificarArticuloControlador.java
@@ -0,0 +1,15 @@
+package com.example.pirristation.Controladores;
+
+import javafx.event.ActionEvent;
+
+public class modificarArticuloOnAction {
+
+    public void editarOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void volverOnAction(ActionEvent actionEvent) {
+
+
+    }
+}
